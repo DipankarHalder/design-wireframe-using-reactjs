21512fa1195cc7da47a5911ae1ac1a19ef9fd8a7
SoftEng-Front <dipankar-halder@outlook.com>
2021-03-16 11:43:57 +0530

    modified respective manifest, index.html and other files as per the project name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import Router from './components/Router';
 import './style/style.scss';
 
-const renderElement = document.getElementById('root');
-ReactDOM.render(<Router />, renderElement);
+const renderElement = document.getElementById('application');
+render(<Router />, renderElement);
